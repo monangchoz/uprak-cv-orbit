173b91dff1f2d06716d69f265af0fb1daf22c48b
ekarinilathifah <ekarini.lathifah123@gmail.com>
2023-05-11 22:31:42 +0700

    update

--- app/fungsi.py
+++ app/fungsi.py
@@ -1,25 +1,35 @@
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Activation, Dropout,LeakyReLU
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
 
 def make_model():
+    #define the convnet
     model = Sequential()
-    model.add(Conv2D(16, (3, 3), input_shape=(32, 32, 3), padding='same'))
-    model.add(LeakyReLU(0.1))
-    model.add(Conv2D(32, (3, 3), padding='same'))
-    model.add(LeakyReLU(0.1))
+    
+    # CONV => RELU => CONV => RELU => POOL => DROPOUT
+    model.add(Conv2D(32, (3, 3), padding='same',input_shape=x_train.shape[1:]))
+    model.add(Activation('relu'))
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
-    model.add(Conv2D(32, (3, 3), padding='same'))
-    model.add(LeakyReLU(0.1))
+    
+    # CONV => RELU => CONV => RELU => POOL => DROPOUT
     model.add(Conv2D(64, (3, 3), padding='same'))
-    model.add(LeakyReLU(0.1))
+    model.add(Activation('relu'))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
+    
+    # FLATTERN => DENSE => RELU => DROPOUT
     model.add(Flatten())
-    model.add(Dense(256))
-    model.add(LeakyReLU(0.1))
+    model.add(Dense(512))
+    model.add(Activation('relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(10, activation='softmax'))
+    
+    # a softmax classifier
+    model.add(Dense(NUM_CLASSES))
+    model.add(Activation('softmax'))
     
     return model
